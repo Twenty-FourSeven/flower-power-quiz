64abe438c5010f9672968ed8f0c3230186850f4b
unknown <thomas.james.1@hotmail.com>
2023-02-27 03:39:00 -0800

    render questions function, for loop to display answer options, add or subtract time

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,39 +1,39 @@
 var quizQuestions = [
-    {
-      question: "Ohana means_______",
-      answers: ["bicycle", "a house", "your family"],
-      correct: "your family",
-    },
-    {
-      question: "What is one thing all Hawaiians do when they enter a home?",
-      answers: ["Bow", "Do a spin", "Take of their shoes"],
-      correct: "Take of their shoes",
-    },
-    {
-      question: "What is Hurricane Popcorn?",
-      answers: [
-        "A nickname for a snow cone",
-        "Popcorn with Furikake and Arare",
-        "Red popcorn ball",
-      ],
-      correct: "Popcorn with Furikake and Arare",
-    },
-    {
-      question: "What does it mean when something is ono?",
-      answers: ["cool, sweet", "silly, frivolous", "delicious, scrumptious"],
-      correct: "cool, sweet",
-    },
-    {
-      question: "In the winter, where would you find the biggest waves?",
-      answers: ["The North Shore", "Kawai", "Honolulu"],
-      correct: "The North Shore",
-    },
-    {
-      question: "Hawaii was the ______ state to be admitted into the union.",
-      answers: ["45th", "49th", "50th"],
-      correct: "50th",
-    },
-  ];
+  {
+    question: "Ohana means_______",
+    answers: ["bicycle", "a house", "your family"],
+    correct: "your family",
+  },
+  {
+    question: "What is one thing all Hawaiians do when they enter a home?",
+    answers: ["Bow", "Do a spin", "Take of their shoes"],
+    correct: "Take of their shoes",
+  },
+  {
+    question: "What is Hurricane Popcorn?",
+    answers: [
+      "A nickname for a snow cone",
+      "Popcorn with Furikake and Arare",
+      "Red popcorn ball",
+    ],
+    correct: "Popcorn with Furikake and Arare",
+  },
+  {
+    question: "What does it mean when something is ono?",
+    answers: ["cool, sweet", "silly, frivolous", "delicious, scrumptious"],
+    correct: "cool, sweet",
+  },
+  {
+    question: "In the winter, where would you find the biggest waves?",
+    answers: ["The North Shore", "Kawai", "Honolulu"],
+    correct: "The North Shore",
+  },
+  {
+    question: "Hawaii was the ______ state to be admitted into the union.",
+    answers: ["45th", "49th", "50th"],
+    correct: "50th",
+  },
+];
 
 var startBtn = document.querySelector(".start-btn");
 var startContainer = document.querySelector(".start-container");
@@ -50,30 +50,53 @@ startBtn.addEventListener("click", function () {
 });
 
 function startTimer() {
+  timerEl.textContent = time;
+  var timerInveral = setInterval(() => {
+    time--;
     timerEl.textContent = time;
-    var timerInveral = setInterval(() => {
-      time--;
-      timerEl.textContent = time;
+
+    if (time === 0 || questionIndex > quizQuestions.length - 1) {
+      clearInterval(timerInveral);
+      endQuiz();
+    }
+  }, 1000);
+}
+
+function renderQuestion() {
+    if (questionIndex > quizQuestions.length - 1) return;
+    var h1 = document.createElement("h1");
+    h1.textContent = quizQuestions[questionIndex].question;
+    quizContainer.append(h1);
   
-      if (time === 0 || questionIndex > quizQuestions.length - 1) {
-        clearInterval(timerInveral);
-        endQuiz();
-      }
-    }, 1000);
+    // for loop - generate and display possible answer options for current question
+    for (var i = 0; i < quizQuestions[questionIndex].answers.length; i++) {
+      var btn = document.createElement("button");
+      btn.textContent = quizQuestions[questionIndex].answers[i];
+      quizContainer.append(btn);
+  
+      // add event listener to answer btn, and when clicked check if right or wrong; then call renderQuestion
+  
+      btn.addEventListener("click", function () {
+        var messageDiv = document.createElement("div");
+        if (this.textContent === quizQuestions[questionIndex].correct) {
+          // Add 10 seconds and display message when answer is correct
+          score += 10;
+          quizContainer.innerHTML = "<p>Correct!</p>";
+        } else {
+          // Subtract 10 seconds and display message when answer is incorrect
+          time -= 10;
+          quizContainer.innerHTML =
+            "<p>Incorrect answer. Ten seconds is deducted from your score.</p>";
+        }
+        // add container and delay for rendering next question
+        quizContainer.appendChild(messageDiv);
+        setTimeout(function () {
+          quizContainer.innerHTML = "";
+          questionIndex++;
+          renderQuestion();
+        }, 0);
+      });
+    }
   }
 
-  submitBtn.addEventListener("click", function () {
-    const userData = {
-      name: input.value,
-      finalScore: score,
-    };
-    let storage = JSON.parse(localStorage.getItem("userScores"));
-    if (storage === null) {
-      storage = [];
-    }
-    storage.push(userData);
 
-    localStorage.setItem("userScores", JSON.stringify(storage));
-    window.location.href = "highscores.html";
-  });
-}
