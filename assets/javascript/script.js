bfc3e7979a98afb015a6183cf3762e05d52e0a0d
unknown <thomas.james.1@hotmail.com>
2023-02-27 02:16:35 -0800

    add quiz questions

---
+++ assets/javascript/script.js
@@ -0,0 +1,37 @@
+var quizQuestions = [
+    {
+      question: "Ohana means_______",
+      answers: ["bicycle", "a house", "your family"],
+      correct: "your family",
+    },
+    {
+      question: "What is one thing all Hawaiians do when they enter a home?",
+      answers: ["Bow", "Do a spin", "Take of their shoes"],
+      correct: "Take of their shoes",
+    },
+    {
+      question: "What is Hurricane Popcorn?",
+      answers: [
+        "A nickname for a snow cone",
+        "Popcorn with Furikake and Arare",
+        "Red popcorn ball",
+      ],
+      correct: "Popcorn with Furikake and Arare",
+    },
+    {
+      question: "What does it mean when something is ono?",
+      answers: ["cool, sweet", "silly, frivolous", "delicious, scrumptious"],
+      correct: "cool, sweet",
+    },
+    {
+      question: "In the winter, where would you find the biggest waves?",
+      answers: ["The North Shore", "Kawai", "Honolulu"],
+      correct: "The North Shore",
+    },
+    {
+      question: "Hawaii was the ______ state to be admitted into the union.",
+      answers: ["45th", "49th", "50th"],
+      correct: "50th",
+    },
+  ];
+  
